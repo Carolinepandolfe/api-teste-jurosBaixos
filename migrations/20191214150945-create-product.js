0e9fc7d037d95a724f84f33503f23cfa954d64e0
Caroline Pandolfe <carolinepandolfe@yahoo.com.br>
2019-12-14 12:21:12 -0300

    Criar tabelas

---
+++ migrations/20191214150945-create-product.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      bar_code: {
+        type: Sequelize.STRING
+      },
+      productPicture: {
+        type: Sequelize.STRING
+      },
+      inStock: {
+        type: Sequelize.INTEGER
+      },
+      category: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('products');
+  }
+};
---
+++ models/product.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const product = sequelize.define('Product', {
+    name: DataTypes.STRING,
+    bar_code: DataTypes.STRING,
+    productPicture: DataTypes.STRING,
+    inStock: DataTypes.INTEGER,
+    category: DataTypes.INTEGER
+  }, {});
+  product.associate = function(models) {
+    // associations can be defined here
+  };
+  return product;
+};
