b8c7796634e3136d6c740a0a4acd34f3365fe105
Carolinepandolfe <carolinepandolfe@yahoo.com.br>
2019-12-15 19:13:30 -0300

    Instalar nodemon e alterar rotas usuários

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const {User} = require('./../models');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/users', function(req, res, next) {
   User.findAll().then(function(users){
     if(users){
       res.send(users)
@@ -14,7 +14,7 @@ router.get('/', function(req, res, next) {
   })
 });
 
-router.post('/', function(req, res, next){
+router.post('/users', function(req, res, next){
   User.create(req.body).then(function(user){
     if(user){
       res.send(user)
